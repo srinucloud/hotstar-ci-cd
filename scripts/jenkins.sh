766c5247bf660a3efc958920323022593f0dea04
srinucloud <srinuengr@gmail.com>
2026-07-13 14:49:06 -0400

    updated readme

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -2,13 +2,15 @@
 #this Script belong to Cloudaseem Youtube channel #####
 # jenkins installation on ubuntu 
 sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre -y
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo apt install fontconfig openjdk-21-jre
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y 
+sudo apt update -y
+sudo apt install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
